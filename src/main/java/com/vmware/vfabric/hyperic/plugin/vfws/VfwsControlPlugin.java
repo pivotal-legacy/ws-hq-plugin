ea42625ad059dd6ed093c04fc0f05ba65b4205de
Wes Schlichter <wschlichter@pivotal.io>
2015-09-10 19:24:52 -0500

    Add license header for GPL v2

--- src/main/java/com/vmware/vfabric/hyperic/plugin/vfws/VfwsControlPlugin.java
+++ src/main/java/com/vmware/vfabric/hyperic/plugin/vfws/VfwsControlPlugin.java
@@ -1,31 +1,22 @@
-/*
- * NOTE: This copyright does *not* cover user programs that use HQ
- * program services by normal system calls through the application
- * program interfaces provided as part of the Hyperic Plug-in Development
- * Kit or the Hyperic Client Development Kit - this is merely considered
- * normal use of the program, and does *not* fall under the heading of
- * "derived work".
- * 
- * Copyright (C) [2004, 2005, 2006], Hyperic, Inc.
- * This file is part of HQ.
- * 
- * HQ is free software; you can redistribute it and/or modify
- * it under the terms version 2 of the GNU General Public License as
- * published by the Free Software Foundation. This program is distributed
- * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA.
- */
+//  Hyperic plugin for vFabric/Pivotal Web Server
+//  Copyright (C) 2012-2015, Pivotal Software, Inc
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-/*
- * This was originally ApacheControlPlugin.java from apache-plugin
- */
+//
+//This file is heavily based on ApacheControlPlugin.java from apache-plugin
+//
 
 package com.vmware.vfabric.hyperic.plugin.vfws;
 
